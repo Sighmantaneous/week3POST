32621de723c49a4a18d26256ca17591af3161623
sighman <g00286398@atu.ie>
2025-10-01 13:31:38 +0100

    Added getProduct and addProduct

--- src/main/java/ie/atu/week3cicdlab/ProductController.java
+++ src/main/java/ie/atu/week3cicdlab/ProductController.java
@@ -2,16 +2,34 @@ package ie.atu.week3cicdlab;
 
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/product")
 @RestController
 public class ProductController {
 
+    List<Product> myList = new ArrayList<>();
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello";
+    }
+
+    @GetMapping("/getproduct")
+    public Product get_product(){
+
+        Product myProduct = new Product("Fridge", 450);
+
+        return myProduct;
+    }
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product myProduct){
+
+        myList.add(myProduct);
+
+        return myProduct;
     }
 }
