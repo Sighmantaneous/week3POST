7da3cb70fe29ea2f4a9e43eb2c99c41d5d4bd193
sighman <g00286398@atu.ie>
2025-10-01 13:15:51 +0100

    Setting up project

---
+++ src/main/java/ie/atu/week3cicdlab/Week3CicDlabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicdlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3CicDlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3CicDlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3cicdlab/Week3CicDlabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicdlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3CicDlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
